305d388cf11a6374d1575f326b181161c0c8eb77
dantavious <dantavious313@gmail.com>
2023-03-26 03:25:43 -0400

    Update network-hd.module.ts

--- src/network-hd/network-hd.module.ts
+++ src/network-hd/network-hd.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TelnetCommsServiceService } from './telnet-comms-service/telnet-comms-service.service';
-import { NetworkHdGateway } from './network-hd/network-hd.gateway';
+import { NetworkHDGateway } from './network-hd/network-hd.gateway';
 
 @Module({
-  providers: [TelnetCommsServiceService, NetworkHdGateway]
+  providers: [TelnetCommsServiceService, NetworkHDGateway]
 })
 export class NetworkHdModule {}
